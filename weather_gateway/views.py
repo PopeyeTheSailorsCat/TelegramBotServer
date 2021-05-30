75d4c7567e37c46dda45b6cc28e9cbb98e476c05
Dmitroved <dmitroved@gmail.com>
2021-05-31 02:12:35 +0300

    add django server part

---
+++ weather_gateway/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+open_weather_token = "9bf05fd8bdd48497c327eb13ac7727fd"
+
+
+class OpenWeatherView(APIView):
+    def get(self, request):
+        # data = request.json()
+        city = request.GET['city']
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric")
+        return Response(r)
